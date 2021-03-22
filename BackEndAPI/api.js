246526bacf9577000038d8f737d51fa292a9d5eb
Luisbv14 <luisbrenesv27@gmail.com>
2021-03-22 02:18:33 -0700

    Solucionado un bug en api.js por haber movido de carpeta paises.js

--- BackEndAPI/api.js
+++ BackEndAPI/api.js
@@ -1,46 +1,44 @@
-var Db = require('./dboperations'); 
-var Paises = require('./paises'); 
+var Db = require('./dboperations');
+var Paises = require('./models/Paises');
 const dboperations = require('./dboperations');
 
-var express = require('express'); 
+var express = require('express');
 var bodyParser = require('body-parser');
-var cors = require('cors'); 
+var cors = require('cors');
 const { response } = require('express');
-var app = express(); 
-var router = express.Router(); 
+var app = express();
+var router = express.Router();
 
-app.use(bodyParser.urlencoded({extended: true})); 
-app.use(bodyParser.json()); 
-app.use(cors()); 
-app.use('/api', router);  
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', router);
 
 router.use((request, response, next) => {
-    console.log('middleware'); 
+    console.log('middleware');
     next();
 })
 
 router.route('/paises').get((request, response) => {
-    dboperations.getPaises().then(result => {        
-        response.json(result[0]); 
+    dboperations.getPaises().then(result => {
+        response.json(result[0]);
     })
 })
 
 router.route('/paises/:id').get((request, response) => {
-    dboperations.getPais(request.params.id).then(result => {        
-        response.json(result[0]); 
+    dboperations.getPais(request.params.id).then(result => {
+        response.json(result[0]);
     })
 })
 
 router.route('/paises').post((request, response) => {
-    let pais = {...request.body}
+    let pais = {...request.body }
 
     dboperations.addPais(pais).then(result => {
-        response.status(201).json(result); 
+        response.status(201).json(result);
     })
 })
 
-var port = process.env.PORT || 8090; 
-app.listen(port); 
-console.log('Paises API is running at ' + port); 
-
-
+var port = process.env.PORT || 8090;
+app.listen(port);
+console.log('Paises API is running at ' + port);
