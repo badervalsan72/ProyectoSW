a70a70c4fcc2d1544eef6eb56a51045dc0de7cc3
Luisbv14 <luisbrenesv27@gmail.com>
2021-03-24 13:50:44 -0700

    añadiendo el registro del frontend

--- FrontEnd/js/registro.js
+++ FrontEnd/js/registro.js
@@ -1,6 +1,26 @@
 var axios = require('axios')
 const { get } = require('https')
 
+const postUsuario = function(TheUrl, nombreUsuario) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var contenido = JSON.stringify({
+        "username": nombreUsuario
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: contenido,
+
+    };
+
+    fetch(TheUrl, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
 
 async function registrarse() {
     let nombre = document.getElementById('nombre').value;
@@ -15,20 +35,31 @@ async function registrarse() {
 
     const config = {
         method: 'post',
-        url: validarUsuariosUrl,
+        url: 'http://localhost:8090/api/Usuarios/validarUsuario',
+        //url: validarUsuariosUrl,
         data: {
-            'username': username
+            username: username
         }
     }
-    await axios(config).then(value => {
+
+    console.log("username: " + username);
+
+    console.log("config: " + config);
+
+    //REVISAR POR QUE ES COMO SI LA CONSULTA DE AXIOS(CONFIG) NUNCA SE COMPLETARA
+
+    /*await axios(config).then(value => {
         console.log('REGISTRO00000000000000000000000');
         console.log(value.data);
         if (value.data.cantusers > 0) {
 
-            alert('Usuario existente.');
+            console.log("Usuario ya existente");
 
         } else {
+            console.log("Usuario nuevo");
             alert('Nuevo usuario!');
         }
-    }).catch(err => { console.log(err) })
+    }).catch(err => { console.log(err) })*/
+
+    postUsuario(validarUsuariosUrl, username);
 }
